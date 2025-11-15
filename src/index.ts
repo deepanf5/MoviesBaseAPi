73a926fa3f8f0250c26a13aabd64aa05f17abe97
deepan <deepanappu1999@gmail.com>
2025-11-15 11:58:26 +0530

    setting up the project

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express, { Application } from 'express';
+dotenv.config();
+
+const APP:Application = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+
+/*** MiddleWare ***/ 
+APP.use(express.json())
+
+/*** Test Route ***/ 
+APP.get('/', (req, res) => {
+  res.send('Hello, TypeScript + Node.js + Express + Mongo!');
+});
+
+/*** Validate environment variables ***/ 
+if (!MONGO_URI) {
+  console.error('MONGO_URI is not defined in your .env file');
+  process.exit(1);
+}
+
+/*** Connect to MongoDB ***/ 
+const connectDB = async () => {
+    await mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+  });
+}
+
+
+/*** Start the Server ***/ 
+const startServer = async () => {
+    await connectDB()
+
+     // Start listening for requests
+    APP.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+
+
+startServer()
+
+
+
+
+
+
