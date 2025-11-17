6b942bb84307d061025a31707ed4846cd98647f0
deepan <deepanappu1999@gmail.com>
2025-11-17 21:18:31 +0530

    adding delete, update, get controller

--- src/controller/movie.controller.ts
+++ src/controller/movie.controller.ts
@@ -12,7 +12,7 @@ router.post('/', async (req: Request, res: Response) => {
 
     try {
 
-        const { title, director, releaseYear, genre, rating } = req.body as IMovie;
+        const { title, director, releaseYear, genre, rating, duration, language, country, cast, description, budget, boxoffice } = req.body as IMovie;
         if (!title || !director || !releaseYear) {
             res.status(403).json({
                 "status": 403,
@@ -26,7 +26,15 @@ router.post('/', async (req: Request, res: Response) => {
             director,
             releaseYear,
             genre,
-            rating
+            rating,
+            duration,
+            language,
+            country,
+            cast,
+            description,
+            budget,
+            boxoffice,
+            createAt: Date
         })
         const result = await newMovie.save()
         res.status(201).json({ result, message: 'Movie Added successfully' })
@@ -49,22 +57,94 @@ router.get('/', async (req: Request, res: Response) => {
     try {
         const movie = await Movie.find().populate('title')
         if (movie) {
-             res.status(200).json(movie); 
+            res.status(200).json(movie);
         }
     }
-    catch(err:any) {
+    catch (err: any) {
         res.status(500).json(
             {
-             status: 500,
-            error: 'Internal Server Error',
-            message: err.message,
-            ReqError: err.message
+                status: 500,
+                error: 'Internal Server Error',
+                message: err.message,
+                ReqError: err.message
             }
         )
     }
 
 
 })
+
+router.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const movie = await Movie.findById(req.params.id)
+        if (movie) {
+            res.status(200).json({movie, message: 'Movie fetched successfully' });
+        }
+    }
+    catch (err: any) {
+        res.status(500).json(
+            {
+                status: 500,
+                error: 'Internal Server Error',
+                message: err.message,
+                ReqError: err.message
+            }
+        )
+    }
+
+
+})
+
+router.put('/:id', async (req: Request, res: Response) => {
+
+    try {
+        const movie = await Movie.findByIdAndUpdate(req.params.id, {
+            title: req.params.title,
+            director: req.params.director,
+            releaseYear: req.params.releaseYear,
+            genre: req.params.genre,
+            rating: req.params.rating,
+            duration: req.params.duration,
+            language: req.params.language,
+            country: req.params.country,
+            cast: req.params.cast,
+            description: req.params.description,
+            budget: req.params.budget,
+            boxoffice: req.params.boxoffice,
+        }, { new: true, runValidators: true })
+           res.status(200).json({movie, message: 'Movie fetched successfully'});
+
+    }
+    catch (err: any) {
+        res.status(500).json(
+            {
+                status: 500,
+                error: 'Internal Server Error',
+                message: err.message,
+                ReqError: err.message
+            }
+        )
+
+    }
+})
+
+
+router.delete('/:id', async (req:Request, res:Response) => {
+
+    try{
+         const movie = await Movie.findByIdAndDelete(req.params.id)
+          res.status(200).json({movie, message: 'Movie Deleted successfully'});
+    }
+    catch(err:any) {
+      res.status(500).json(
+            {
+                status: 500,
+                error: 'Internal Server Error',
+                message: err.message,
+                ReqError: err.message
+            }
+        )
+}})
 
 
 export default router
