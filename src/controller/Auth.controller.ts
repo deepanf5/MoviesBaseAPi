49e6edb185cc2aeb768ae5545ac8e74d3ab34f3a
deepan <deepanappu1999@gmail.com>
2025-11-26 21:54:08 +0530

    adding the authentication

---
+++ src/controller/Auth.controller.ts
@@ -0,0 +1,49 @@
+import bcrypt from "bcryptjs";
+import { Request, Response, } from "express";
+import express from 'express';
+import Admin, { IAdmin } from "../models/Admin.model";
+const router = express.Router()
+
+router.post('/', async (req: Request, res: Response) => {
+
+    try {
+
+
+        const { email, password } = req.body
+        if (!email
+            || !password
+        ) {
+            res.status(403).json({
+                "status": 403,
+                "error": "Error",
+                "message": "Eamil or password  or isMoiveBaseAdmin are required'"
+            })
+        }
+
+        let user = await Admin.findOne({ email }) as IAdmin
+
+        if (!user) {
+            res.status(400).json({ message: 'Invalid email or Password' })
+        }
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        // Success
+        res.status(200).json({ message: "Login successful" });
+    }
+    catch (err: any) {
+        res.status(500).json(
+            {
+                status: 500,
+                error: 'Internal Server Error',
+                message: err.message,
+                ReqError: err.message
+            }
+        )
+
+    }
+
+})
