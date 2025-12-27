a097b5c039bac350ae1b37367edd9dc4781ca62e
deepan <deepanappu1999@gmail.com>
2025-12-27 11:41:32 +0530

    adding the new auth logic

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,20 +1,71 @@
 import dotenv from 'dotenv';
 import { Request, Response, NextFunction } from "express";
+import { jwtVerify } from 'jose';
 dotenv.config();
 
-export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
 
-    const apiKey = req.headers['x-api-key']
+const secret = new TextEncoder().encode(process.env.jwtPrivateKey);
 
-    if (apiKey && apiKey === process.env.ADMIN_API_KEY) {
-         next()
+export const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+
+    // const authHeader  = req.headers['authorization']
+
+
+    // if (!authHeader) {
+    //   return res.status(401).json({
+    //     status: 401,
+    //     error: 'Unauthorized',
+    //     message: 'No token provided'
+    //   });
+    // }
+
+    // if (authHeader) {
+    //     const { payload } = await jwtVerify(authHeader, secret);
+    //     console.log(payload)
+    //     if(payload.role) {
+    //         next()
+    //     }
+    // }
+    // else {
+    //     res.status(403).json({
+    //     "status": 403,
+    //     "error": "Forbidden",
+    //     "message": "You do not have permission to access this resource."
+    // })
+
+    // }
+
+    try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader) {
+            return res.status(401).json({
+                status: 401,
+                error: 'invalid user or',
+                message: 'Invalid or expired token'
+            });
+        }
+        const { payload } = await jwtVerify(authHeader, secret);
+
+        if (payload.adminRole) {
+            next()
+
+        }
     }
-    else {
-        res.status(403).json({
-        "status": 403,
-        "error": "Forbidden",
-        "message": "You do not have permission to access this resource."
-    })
+    catch (err: any) {
 
+          if (err.code === 'ERR_JWT_EXPIRED') {
+      return res.status(401).json({
+        status: 401,
+        error: 'Unauthorized',
+        message: 'Token has expired'
+      });
+    }
+
+        return res.status(401).json({
+            status: 401,
+            error: 'Unauthorized',
+            message: 'Invalid or expired token'
+        });
     }
 }
